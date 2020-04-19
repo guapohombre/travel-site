2a6332e3f4d0e54284ada518758363118e0a9b22
Tom <thomasdcorrell@gmail.com>
2020-04-19 08:21:45 -0700

    completed sticky header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,13 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js';
 import RevealOnScroll from './modules/Reveal_on_scroll.js';
-
+import StickyHeader from './modules/sticky-header.js';
 
 if (module.hot) {
   module.hot.accept();
 }
 
-
+let stickyHeader = new StickyHeader();
 let mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'),75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'),60);
